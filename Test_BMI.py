b70c5a13791ff78d36ff53faddbe74eab370b3fa
LaMayaNewsome <110430068+LaMayaNewsome@users.noreply.github.com>
2024-03-07 09:34:32 -0600

    finished app

---
+++ Test_BMI.py
@@ -0,0 +1,22 @@
+import unittest
+#import BMI.py
+
+class TestBMICalculator(unittest.Testcase):
+    def test_calculateBMI(self):
+        #Test cases for calculate_bmi function
+        self.assertAlmostEqual(calculateBMI(5, 9, 160), 23.6, places=1) #Normal weight
+        self.assertAlmostEqual(calculateBMI(6, 0, 200), 27.1, places=1) #Overweight
+        self.assertIsNone(calculateBMI(0, 0, 0)) #Invalid input
+
+    def test_getCategory(self):
+        #Test cases for get_bmi_category
+        self.assertEqual(getCategory(21), "Underweight")
+        self.assertEqual(getCategory(21), "Normal weight")
+        self.assertEqual(getCategory(27), "Overweight")
+        self.assertEqual(getCategory(21), "Obese")
+        self.assertEqual(getCategory(None), "Invalid Innput")
+
+    if __name__ == "__main__":
+        unittest.main()
+
+    
